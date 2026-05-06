cdbc0e1490fe23c9db0b4728801f637f5e882d5e
Jimmy_Duarte_Ramirez <jimmy.duarte86@gmail.com>
2026-05-06 14:51:35 -0400

    se agrega MONGO_URI a db.js se modifica index.js para puerto de db

--- src/db.js
+++ src/db.js
@@ -1,11 +1,10 @@
-import mongoose from 'mongoose'
-
+import mongoose from 'mongoose';
 
 export const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb+srv://qodeya:<db_password>@cluster0.1ukukar.mongodb.net/?appName=Cluster0/merndb')
-        console.log(">>>>> DB is connected")
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log(">>>>> DB is connected");
+  } catch (error) {
+    console.log(error);
+  }
 };
