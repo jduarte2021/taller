aa254cb548d6d205b9ade42c1f40bc77c1c9d382
Jimmy_Duarte_Ramirez <jimmy.duarte86@gmail.com>
2026-05-06 14:06:06 -0400

    Se agrega url de DB en backend y URL de front en CORS

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ import pdfRoutes from "./routes/pdf.routes.js";
 const app = express();
 const __dirname = path.resolve();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://taller-ten-amber.vercel.app', credentials: true }));
 app.use(morgan('dev'));
 app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost/merndb')
+        await mongoose.connect('mongodb+srv://qodeya:<db_password>@cluster0.1ukukar.mongodb.net/?appName=Cluster0/merndb')
         console.log(">>>>> DB is connected")
     } catch (error) {
         console.log(error);
