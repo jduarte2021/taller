fe756115ae9d1daa3ad2dd4d7e46a3a64e2574d1
Jimmy_Duarte_Ramirez <jimmy.duarte86@gmail.com>
2026-05-06 15:09:18 -0400

    se modifica db.js quitando MONGO_URI y escribiendo rul completa para prueba

--- src/db.js
+++ src/db.js
@@ -1,8 +1,21 @@
+// import mongoose from 'mongoose';
+
+// export const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI);
+//     console.log(">>>>> DB is connected");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
 import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(
+      'mongodb+srv://qodeya:5ynQ4W0Z8h@cluster0.1ukukar.mongodb.net/merndb?retryWrites=true&w=majority'
+    );
     console.log(">>>>> DB is connected");
   } catch (error) {
     console.log(error);
