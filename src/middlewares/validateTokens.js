18b2b97247f984a71ae4b11a6bbce8c07e66dbf0
Jimmy_Duarte_Ramirez <jimmy.duarte86@gmail.com>
2026-05-07 11:57:11 -0400

    Rollback en modificacion validateToken.js con (process.env.TOKEN_SECRET) para que tome token guardado en ambiente

--- src/middlewares/validateTokens.js
+++ src/middlewares/validateTokens.js
@@ -7,7 +7,7 @@ export const authRequired = (req,res,next) => {
     if (!token)
         return res.status(401).json({ message: "No hay token, autorizacion denegada"});
 
-        jwt.verify(token, (process.env.TOKEN_SECRET), (err, user) =>{
+        jwt.verify(token,TOKEN_SECRET, (err, user) =>{
             if (err) return res.status(403).json({ message: "Token invalido"});
             
             req.user = user
