cfef4285df2579b0ee6d7fd8c367373cd7b77645
Jimmy_Duarte_Ramirez <jimmy.duarte86@gmail.com>
2026-05-15 23:08:55 -0400

    eliminado

--- taller/src/config.js
+++
@@ -1,1 +0,0 @@
-export const TOKEN_SECRET = 'ThinkPad@551v'
--- taller/src/schemas/api/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
-  withCredentials: true,  // CRÍTICO: envía cookie en cada request
-});
-
-export default instance;
