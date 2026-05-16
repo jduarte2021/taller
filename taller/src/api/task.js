698a6b7d05b5b65f719a2c2a5c2d88a44453016e
Jimmy_Duarte_Ramirez <jimmy.duarte86@gmail.com>
2026-05-15 20:09:06 -0400

    nueva correccion api

--- taller/src/api/task.js
+++ taller/src/api/task.js
@@ -1,23 +1,7 @@
 import axios from "./axios";
 
-// export const getTasksRequest = () => axios.get(`/tasks`);
-export const getTasksRequest = () => 
-    axios.get(`/tasks`, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-      },
-    });
-
-export const getTaskRequest = (id) => axios.get(`/tasks/${id}`);
-
-// export const createTaskRequest = (task) => axios.post(`/tasks`, task);
-export const createTaskRequest = (task) => 
-    axios.post(`/tasks`, task, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-      },
-    });
-
+export const getTasksRequest  = () => axios.get("/tasks");
+export const getTaskRequest   = (id) => axios.get(`/tasks/${id}`);
+export const createTaskRequest = (task) => axios.post("/tasks", task);
 export const updateTaskRequest = (task) => axios.put(`/tasks/${task.id}`, task);
-
 export const deleteTaskRequest = (id) => axios.delete(`/tasks/${id}`);
